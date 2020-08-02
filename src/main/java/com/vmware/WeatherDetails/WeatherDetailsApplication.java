0ec562fc3ebfec1b33b31bfeeb802eec932c1560
Pavithra Rammohan <haripavi95@gmail.com>
2020-08-02 15:07:46 -0400

    Changed Sout to Logger

--- src/main/java/com/vmware/WeatherDetails/WeatherDetailsApplication.java
+++ src/main/java/com/vmware/WeatherDetails/WeatherDetailsApplication.java
@@ -1,16 +1,19 @@
 package com.vmware.WeatherDetails;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@Slf4j
 public class WeatherDetailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherDetailsApplication.class, args);
-		System.out.println("hello");
+		log.info("hello");
+
 	}
 
 }
